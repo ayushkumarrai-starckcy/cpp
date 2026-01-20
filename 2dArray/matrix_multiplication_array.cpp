294fe5ae8cf3ff8a9a51eace8ab333d94b18f941
ayushkumarrai-starckcy <thisisayushkumarrai@gmail.com>
2026-01-20 18:34:24 +0530

    Add matrix multiplication implementation in C++
    Added matrix_multiplication_array.cpp with a program to multiply two matrices using arrays and output the result. Included corresponding executable files.

---
+++ 2dArray/matrix_multiplication_array.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n, p;
+
+    // A: m x n
+    // B: n x p
+    cin >> m >> n >> p;
+
+    int A[100][100], B[100][100], C[100][100];
+
+    // Input matrix A
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    // Input matrix B
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < p; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    // Initialize result matrix C
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            C[i][j] = 0;
+        }
+    }
+
+    // Matrix multiplication
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // Output result matrix C
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
